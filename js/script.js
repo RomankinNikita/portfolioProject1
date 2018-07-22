c5180ddf0ccd2103218a41d67146e02e1260d352
RomankinNikita <41050397+RomankinNikita@users.noreply.github.com>
2018-07-22 22:27:29 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(function(){
+	var btn = $('.nav-tab-list__btn');
+	var item = $('.nav-tab-list__item');
+	var tab = $('.tab-cont__item');
+
+	btn.click(function(){
+		$(this).parent().addClass('active');
+		$(this).parent().siblings().removeClass('active');
+		if ($(this).hasClass('graphic')) {
+			tab.hide();
+			$('[id *= tab_1]').show();
+		} else if ($(this).hasClass('web')) {
+			tab.hide();
+			$('[id *= tab_2]').show();
+		} else if ($(this).hasClass('brand')) {
+			tab.hide();
+			$('[id *= tab_3]').show();
+		} else if ($(this).hasClass('video')) {
+			tab.hide();
+			$('[id *= tab_4]').show();
+		} else if ($(this).hasClass('photo')) {
+			tab.hide();
+			$('[id *= tab_5]').show();
+		} else {
+			tab.show();
+		}
+	});
+})
